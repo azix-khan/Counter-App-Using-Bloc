31f2edd5a6bdf7ed3dfd9bf3868eecb2f8e7bd19
azix khan <azixkhan.55@gmail.com>
2024-05-19 12:30:42 +0500

    improve design

--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -16,41 +16,57 @@ class _CounterScreenState extends State<CounterScreen> {
   Widget build(BuildContext context) {
     print("initial build111");
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 25, 178, 238),
         centerTitle: true,
-        title: const Text('Counter App Using Bloc'),
+        title: const Text(
+          'Counter App Using Bloc',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          BlocBuilder<CounterBloc, CounterState>(builder: (context, state) {
-            print("build");
-            return Text(
-              state.counter.toString(),
-              style: const TextStyle(fontSize: 60),
-            );
-          }),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(IncrementCounter());
-                  },
-                  child: const Text('Increment')),
-              const SizedBox(
-                width: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(DecrementCounter());
-                  },
-                  child: const Text('Decrement')),
-            ],
-          )
-        ],
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 25, 178, 238),
+            Color.fromARGB(255, 21, 236, 229)
+          ],
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterBloc, CounterState>(builder: (context, state) {
+              print("build");
+              return Text(
+                state.counter.toString(),
+                style: const TextStyle(fontSize: 60),
+              );
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      context.read<CounterBloc>().add(IncrementCounter());
+                    },
+                    child: const Text('Increment')),
+                const SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      context.read<CounterBloc>().add(DecrementCounter());
+                    },
+                    child: const Text('Decrement')),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
